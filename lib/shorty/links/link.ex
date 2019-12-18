97ebe90a1f2bcb7c874b6bd6869c73e418e51343
Hüseyin TUNÇ <huseyin.tunc@bulutfon.com>
2019-12-18 23:42:37 +0300

    generate random shortcode if not given

--- lib/shorty/links/link.ex
+++ lib/shorty/links/link.ex
@@ -6,13 +6,14 @@ defmodule Shorty.Links.Link do
     field :shortcode, :string
     field :url, :string
     field :redirect_count, :integer, default: 0
-    timestamps([type: :utc_datetime_usec])
+    timestamps(type: :utc_datetime_usec)
   end
 
   @doc false
   def changeset(link, attrs) do
     link
     |> cast(attrs, [:shortcode, :url])
+    |> set_shortcode
     |> validate_required([:shortcode, :url])
     |> validate_shortcode
     |> validate_url
@@ -20,22 +21,51 @@ defmodule Shorty.Links.Link do
   end
 
   @doc false
-  defp validate_shortcode(changeset) do
-    shortcode = get_field(changeset, :shortcode)
-    if Regex.match?(~r/^[0-9a-zA-Z_]{6}$/, shortcode) do
+  defp set_shortcode(changeset) do
+    if is_nil(get_field(changeset, :shortcode)) do
       changeset
+      |> put_change(:shortcode, random_string(6))
     else
-      add_error(changeset, :shortcode, "Should match with ^[0-9a-zA-Z_]{6}$.")
+      changeset
     end
+
+    # shortcode =
+    #  changeset
+    #  |> get_field(:shortcode, random_string(6))
+
+    # changeset
+    # |> put_change(:shortcode, shortcode)
+  end
+
+  @doc false
+  defp validate_shortcode(changeset) do
+    changeset
+    |> validate_format(:shortcode, ~r/^[0-9a-zA-Z_]{6}$/)
   end
 
   @doc false
   defp validate_url(changeset) do
     url = get_field(changeset, :url)
-    if Regex.match?(~r/^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/, url) do
+
+    if Regex.match?(
+         ~r/^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/,
+         url
+       ) do
       changeset
     else
       add_error(changeset, :url, "It's not a valid url.")
     end
+  end
+
+  @doc false
+  def random_string(length) do
+    chars =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_1234567890"
+      |> String.split("")
+      |> Enum.filter(fn x ->
+        x != ""
+      end)
+
+    Enum.map(1..length, fn _ -> Enum.random(chars) end) |> Enum.join("")
   end
 end
